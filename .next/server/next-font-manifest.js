acab8b653996b823fa46279e159a3e0417c7b79e
isiagi <isiagigeofrey0@gmail.com>
2025-05-21 10:58:18 +0300

    remove .next dir

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/isiagi/Documents/techlab-next/app/layout\":[\"static/media/d9df7be9880a78d0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
